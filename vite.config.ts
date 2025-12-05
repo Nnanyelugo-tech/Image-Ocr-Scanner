0fc2e1a07f36162a7e80694ebf8e2c0af9525225
Nnanyelugo Tech <nnanyelugomeputaife@gmail.com>
2025-12-05 23:10:10 +0100

    fix: added base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   plugins: [
+//     react({
+//       babel: {
+//         plugins: [['babel-plugin-react-compiler']],
+//       },
+//     }),
+//     tailwindcss(),
+//   ],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/Image-Ocr-Scanner/',   // <<< REQUIRED FOR GITHUB PAGES
   plugins: [
     react({
       babel: {
